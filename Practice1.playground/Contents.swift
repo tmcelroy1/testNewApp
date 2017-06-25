6df1d8e4e8ef514ca6bd1fa39753a3b390888c2d
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-25 13:09:57 -0400

    6/25

--- Practice1.playground/Contents.swift
+++ Practice1.playground/Contents.swift
@@ -1,4 +1,4 @@
-var  birthYear = 1966
+var  theLabel = ""
 
 if birthYear < 2000 {
     print("You were born in the 20th Century!")
@@ -6,3 +6,5 @@ if birthYear < 2000 {
 else {
     print("You were born in the 21st Century!")
 }
+
+theLabel.text = "Answer:  \(Double(text1.text!)! + Double(text2.text!)!)"
