d58c60d8ece05139ee14f963ec339ff2302b7f81
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-24 23:09:19 -0400

    2nd

---
+++ Family Activities By State.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+var str1 = "first try"
+print(str)
+print(str1)
+var a = 1
+var b = 5
+
+print (a+b)
+if a+b > 8
+{
+    print("yes")
+}
+else
+{
+    print("no")
+}
