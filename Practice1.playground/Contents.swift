18922fa172ff225048adc0949923653642ff03bd
Tim McElroy1 <tmcelroyt@mac.com>
2017-06-26 14:17:18 -0400

    6/25 Array

--- Practice1.playground/Contents.swift
+++ Practice1.playground/Contents.swift
@@ -1,10 +1,2 @@
-var  theLabel = ""
-
-if birthYear < 2000 {
-    print("You were born in the 20th Century!")
-}
-else {
-    print("You were born in the 21st Century!")
-}
-
-theLabel.text = "Answer:  \(Double(text1.text!)! + Double(text2.text!)!)"
+var favCandyBar = ["Snickers","Milky Way","M&M"]
+favCandyBar.append("Mounds")
